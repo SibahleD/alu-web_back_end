39d6a06e67b1d7bc21e838a4f1db0afb6da45870
SibahleD <s.dlamini2@alustudent.com>
2026-02-21 13:56:07 +0200

    last time

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
+"""
+Main Flask application module.
+
+This module serves HTML pages to users using Flask routes.
+"""
+
 from flask import Flask, render_template
-'''
-Main script serving HTML pages to users
-'''
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
+    """
+    Render the home page.
+
+    Returns:
+        str: Rendered HTML template for the home page.
+    """
     # Serves the html to user
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

4a0205960b9307facbfef4654a6628b5f039ad6b
SibahleD <s.dlamini2@alustudent.com>
2026-02-21 14:35:43 +0200

    vrvr

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -5,7 +5,7 @@ Main Flask application module.
 This module serves HTML pages to users using Flask routes.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 class Config(object):
@@ -20,12 +20,19 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 @app.route('/')
 def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 if __name__ == '__main__':
     app.run()

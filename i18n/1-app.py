02d54bfe3ea6e36ffc2293b629ba0483b069e029
SibahleD <s.dlamini2@alustudent.com>
2026-02-21 14:17:05 +0200

    i18n - 1 - cool

---
+++ i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Main Flask application module.
+
+This module serves HTML pages to users using Flask routes.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()

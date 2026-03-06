3262096f97bc8ae0b9a7980bda353d298c52ff8a
SibahleD <s.dlamini2@alustudent.com>
2026-03-06 12:42:21 +0200

    c:ff

--- E56_basic/0-constants.js
+++ E56_basic/0-constants.js
@@ -1,5 +1,4 @@
 export function taskFirst() {
-<<<<<<< HEAD
     const task = 'I prefer const when I can.';
     return task;
   }
@@ -14,19 +13,3 @@ export function taskFirst() {
   
     return combination;
   }
-=======
-  const task = 'I prefer const when I can.';
-  return task;
-}
-
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
->>>>>>> c0d680cc4a0ad43b6c5d04c4a0d51411306879cc
--- E56_basic/README.md
+++ E56_basic/README.md
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 ALU ES6 BASICS
-=======
 javascript again yay!
->>>>>>> c0d680cc4a0ad43b6c5d04c4a0d51411306879cc

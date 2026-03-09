02e536787d3ee3d5287aaf73fe317b75f88a11ad
SibahleD <s.dlamini2@alustudent.com>
2026-03-09 17:50:19 +0200

    we good 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,8 @@
-export default class ALXCourse{
+export default class ALXCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Name must be a string');
     this._name = name;
     this._length = length;
     this._students = students;

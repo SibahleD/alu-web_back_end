21c87b9a0be23ec55245003d4186cd33920eb543
SibahleD <s.dlamini2@alustudent.com>
2026-03-18 14:07:39 +0200

    ffv

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: process.stdin.isTTY
+  input: process.stdin,
+  output: process.stdout,
 });
 
 readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-    console.log(`Your name is: ${name}`);
-    readline.close();
+  console.log(`Your name is: ${name}`);
+  readline.close();
 });
 
 readline.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
+  console.log('This important software is now closing');
+  process.exit(0);
 });

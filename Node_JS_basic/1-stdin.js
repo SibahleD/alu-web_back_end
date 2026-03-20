19809868cfbe6ba29783c265516ea24bd6c8f578
SibahleD <s.dlamini2@alustudent.com>
2026-03-20 15:09:27 +0200

    fixfix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ const readline = require('readline').createInterface({
 });
 
 readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r`);
   readline.close();
 });
 
